de1176e73523aa5eb91a3c1070e738d8107fbd4a
Karl Doherty <karl.doherty27@mail.dcu.ie>
2022-01-24 15:25:07 +0000

    Added early build of sky sports scraper

--- src/sky_scraper.py
+++ src/sky_scraper.py
@@ -2,28 +2,22 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+url = 'https://www.skysports.com/premier-league-news'
+content = requests.get(url)
+soup = BeautifulSoup(content.text, "html.parser")
+body = soup.find_all("div", "news-list__body")
 
-def main():
+title_list = []
+article_list = []
+titles_plus_links = {}
 
-    url = 'https://www.skysports.com/premier-league-news'
-    content = requests.get(url)
-    soup = BeautifulSoup(content.text, "html.parser")
-    body = soup.find_all("div", "news-list__body")
+for item in body:
+    title = item.find_all('h4', class_="news-list__headline")[0].text
+    title_list.append(title.strip())
+    articles = [i['href'] for i in item.find_all('a', class_="news-list__headline-link")]
+    article_list.append(articles)
 
-    title_list = []
-    article_list = []
-    titles_plus_links = {}
+titles_plus_links = {title_list[i]: article_list[i] for i in range(len(article_list))}
 
-    for item in body:
-        title = item.find_all('h4', class_="news-list__headline")[0].text
-        title_list.append(title.strip())
-        articles = [i['href'] for i in item.find_all('a', class_="news-list__headline-link")]
-        article_list.append(articles)
-
-    titles_plus_links = {title_list[i]: article_list[i] for i in range(len(article_list))}
-
-    for key, value in titles_plus_links.items():
-        print(key + ' : ' + str(value) + '\n\n')
-
-if __name__ == '__main__':
-    main()
+for key, value in titles_plus_links.items():
+    print(key + ' -> ' + str(value)[2:-2] + '\n\n')
