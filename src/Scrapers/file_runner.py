a4f9f371cd1c859fa88ab737c588a54412b3d60b
yury chupahin <yury.chupahin2@mail.dcu.ie>
2022-02-15 18:44:53 +0000

    Added Scrapers for Premier League Table, Sky Sports news, Top Goalscorer and Fixtures

---
+++ src/Scrapers/file_runner.py
@@ -0,0 +1,4 @@
+import time,os
+while 1:
+    time.sleep(15)
+    os.system("start python sky_news_scraper.py")
---
+++ src/Scrapers/premier_league_table.py
@@ -0,0 +1,34 @@
+from datetime import date
+from importlib.resources import contents
+import requests
+import json
+import re
+import firebase_admin
+from firebase import firebase
+from firebase_admin import credentials
+from firebase_admin import db
+import time
+
+cred = credentials.Certificate('firebase-sdk.json')
+firebase_admin.initialize_app(cred, {
+    'databaseURL' : 'https://premnews-99ac4-default-rtdb.europe-west1.firebasedatabase.app/'
+})
+
+ref = db.reference('/table')
+ref.delete()
+
+url = "https://api-football-v1.p.rapidapi.com/v3/standings"
+
+querystring = {"season":"2021","league":"39"}
+
+headers = {
+    'x-rapidapi-host': "api-football-v1.p.rapidapi.com",
+    'x-rapidapi-key': "45cfe9e593msh04cf7c6f66c28c4p150625jsnc8dd59b1efa7"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+content_response = response.content
+content = content_response.decode('utf-8')
+content_json = json.loads(content)
+ref.push(content_json)
+
