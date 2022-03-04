91d6d83513b929998c8aa093079d0d8d9e869d7b
yury chupahin <yury.chupahin2@mail.dcu.ie>
2022-03-04 13:08:22 +0000

    added User Manual

--- src/Scrapers/runner.py
+++ src/Scrapers/runner.py
@@ -1,3 +1,6 @@
+from ast import Try
+from logging import exception
+from tkinter import E
 import firebase_admin
 from firebase import firebase
 from firebase_admin import credentials
@@ -8,12 +11,16 @@ firebase_admin.initialize_app(cred, {
     'databaseURL' : 'https://premnews-99ac4-default-rtdb.europe-west1.firebasedatabase.app/'
 })
 
-import daily_mail
-import official_prem_scraper
-import independent_scraper
-import sky_news_scraper
-
 ref = db.reference('/news')
 db_len = len(ref.order_by_child('Title').get())
 
-print('The scrapers are currently working correctly. ' + '\n' +  'The database currently contains ' + str(db_len) + ' news arcticles from 4 sources')
+try:
+
+    import daily_mail
+    import official_prem_scraper
+    import independent_scraper
+    import sky_news_scraper
+    print('The scrapers are currently working correctly. ' + '\n' +  'The database currently contains ' + str(db_len) + ' news arcticles from 4 sources')
+
+except FileNotFoundError:
+    print("There is a missing file!")
